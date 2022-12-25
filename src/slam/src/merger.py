f12cfb325d961227d7ef7ac584572d2a54464cf2
Mostafa Abdelbrr <mostafa.abdelbrr@hotmail.com>
2022-12-25 22:21:33 +0200

    Update merger.

--- src/slam/src/merger.py
+++ src/slam/src/merger.py
@@ -4,10 +4,10 @@ import rospy
 import message_filters
 from sensor_msgs.msg import LaserScan
 from nav_msgs.msg import Odometry
-from slam.msg import incorporated_sensor_data
+from sensor_incorporation.msg import incorporated_sensor_data
 import math
 
-# front_measurements: point_cloud2runs
+# front_measurements: point_cloud2
 # rear_measurements: point_cloud2
 # odom_measurements: 
 class sensor_incorporation:
@@ -15,7 +15,7 @@ class sensor_incorporation:
         self.front_sub = message_filters.Subscriber('/robot/front_laser/scan', LaserScan)
         self.back_sub = message_filters.Subscriber('/robot/rear_laser/scan', LaserScan)
         self.odom_sub = message_filters.Subscriber('/robot/robotnik_base_control/odom', Odometry)
-        self.sensor_topic = rospy.Publisher('/sensors_topic', incorporated_sensor_data, queue_size=10)
+        self.sensor_topic = rospy.Publisher('/sensors_topic', incorporated_sensor_data, queue_size=1)
         self.timesynced = message_filters.ApproximateTimeSynchronizer([self.front_sub, self.back_sub, self.odom_sub], queue_size=10, slop=0.002)
         # self.timesynced = message_filters.TimeSynchronizer([self.front_sub, self.back_sub, self.odom_sub], queue_size=10)
         self.timesynced.registerCallback(self.merge)
@@ -25,39 +25,47 @@ class sensor_incorporation:
     def merge(self, front_measurements, rear_measurements, odom_measurements):
         self.angles = {}
         merged = incorporated_sensor_data()
-        merged.angle_min = - math.pi
-        merged.angle_max = math.pi
-        merged.angle_increment = 0.5 * math.pi / 180
+        merged.frame_id = 'robot_map'
+        merged.header = odom_measurements.header
+        
+        merged.angle_min = front_measurements.angle_min
+        merged.angle_max = rear_measurements.angle_max
+        merged.angle_increment = 2*front_measurements.angle_increment
         merged.time_increment = front_measurements.time_increment
         merged.scan_time = front_measurements.scan_time
-        merged.range_min = max(front_measurements.range_min, rear_measurements.range_min)
-        merged.range_max = min(front_measurements.range_max, rear_measurements.range_max)
+        merged.range_min = front_measurements.range_min
+        merged.range_max = front_measurements.range_max
         
         
-        merged.ranges = [0]*round(360/0.5)
+        merged.ranges[0:270] = front_measurements.ranges[0:540:2]
+        merged.ranges[270:360] = rear_measurements.ranges[180:360:2]
         
-        # Front measurements:
-        for index in range(len(front_measurements.ranges)):
-            angle = front_measurements.angle_min + index*front_measurements.angle_increment
-            if front_measurements.ranges[index] > front_measurements.range_min and front_measurements.ranges[index] < front_measurements.range_max:
-                self.angles[angle] = front_measurements.ranges[index]
-            else:
-                self.angles[angle] = -1
+        # # Front measurements:
+        # for index in range(len(front_measurements.ranges)):
+        #     angle = front_measurements.angle_min + index*front_measurements.angle_increment
+        #     if front_measurements.ranges[index] > front_measurements.range_min and front_measurements.ranges[index] < front_measurements.range_max:
+        #         self.angles[angle] = front_measurements.ranges[index]
+        #     elif front_measurements.ranges[index] <front_measurements.range_min:
+        #         self.angles[angle] = front_measurements.range_min
+        #     else:
+        #         self.angles[angle] = front_measurements.range_max
 
-        # Rear measurements:
-        for index in range(len(rear_measurements.ranges)):
-            angle = rear_measurements.angle_min + index * rear_measurements.angle_increment + math.pi
-            if angle > math.pi:
-                angle -= 2 * math.pi
-            if angle not in self.angles:
-                if rear_measurements.ranges[index] > rear_measurements.range_min and rear_measurements.ranges[index] < rear_measurements.range_max:
-                    self.angles[angle] = rear_measurements.ranges[index]
-                else:
-                    self.angles[angle] = -1
+        #     # self.angles[angle]= front_measurements.ranges[index]
 
-        for angle in self.angles:
-            index = round((angle - merged.angle_min)/merged.angle_increment)
-            merged.ranges[index] = self.angles[angle]
+        # # Rear measurements:
+        # for index in range(len(rear_measurements.ranges)):
+        #     angle = rear_measurements.angle_min + index * rear_measurements.angle_increment + math.pi
+        #     if angle > math.pi:
+        #         angle -= 2 * math.pi
+        #     if angle not in self.angles:
+        #         if rear_measurements.ranges[index] > rear_measurements.range_min and rear_measurements.ranges[index] < rear_measurements.range_max:
+        #             self.angles[angle] = rear_measurements.ranges[index]
+        #         else:
+        #             self.angles[angle] = -1
+
+        # for angle in self.angles:
+        #     index = round((angle - merged.angle_min)/merged.angle_increment)
+        #     merged.ranges[index] = self.angles[angle]
         
         merged.pose = odom_measurements.pose
         merged.twist = odom_measurements.twist
@@ -71,10 +79,7 @@ def main():
     
     rospy.init_node('sensor_incorporation')
     sensor_data = sensor_incorporation()
-    try:
-        rospy.spin()
-    except:
-        pass
+    rospy.spin()
 
 if __name__ == '__main__':
     try:
